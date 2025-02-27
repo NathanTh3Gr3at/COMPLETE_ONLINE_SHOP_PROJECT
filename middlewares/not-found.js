38f512f6af986448bdaf1195f79867fcc41328bf
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2025-02-27 10:37:05 +0200

    Add not found middleware and enhance cart badge updates: implement 404 handler and update cart badge handling to support multiple elements

---
+++ middlewares/not-found.js
@@ -0,0 +1,6 @@
+function notFoundHandler(req,res){
+    res.status(404).render('shared/404')
+}
+
+
+module.exports=notFoundHandler
--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,5 +1,5 @@
 const addToCartButtonElement = document.querySelector('#product-details button');
-const cartBadgeElement = document.querySelector('.nav-items .badge');
+const cartBadgeElements = document.querySelectorAll('.nav-items .badge');
 
 async function addToCart() {
   const productId = addToCartButtonElement.dataset.productid;
@@ -31,8 +31,10 @@ async function addToCart() {
   const responseData = await response.json();
 
   const newTotalQuantity = responseData.newTotalItems;
-
+for(const cartBadgeElement of cartBadgeElements){
   cartBadgeElement.textContent = newTotalQuantity;
+}
+  
 }
 
 addToCartButtonElement.addEventListener('click', addToCart);
